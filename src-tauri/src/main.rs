32c6f5c595a37c6650d216d122d589a3fb6c489d
wuwei <weiwu.gangfeng56.com>
2022-12-08 14:14:34 +0800

    抖音tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,8 @@
 )]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod command;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -11,7 +13,16 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            command::get_url_id,
+            command::get_video_info_by_id,
+            command::get_video_full_info_by_id,
+            command::download_video,
+            command::get_user_info_by_url,
+            command::get_user_full_info_by_url,
+            command::get_list_by_user_id,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,301 @@
+import { Button, Input, Space, Select, Popover, Table, message, BackTop } from "antd";
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
 import init, { greet } from '@wasm/foo'
-import "./App.css";
+import { open as openFile } from "@tauri-apps/api/shell";
+import { QuestionCircleOutlined, PlaySquareOutlined, GithubFilled, EyeOutlined, DownloadOutlined, CloudDownloadOutlined, CloseCircleFilled } from "@ant-design/icons";
+import imgLogo from "./logo.png";
+import "./App.less";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
-  
-  useEffect(() => {
-    // load wasm file
-    init();
-  }, [])
-
+export default function App() {
+  const [parseType, setParseType] = useState("video");
+  const [url, setUrl] = useState("");
+  const [videoInfo, setVideoInfo] = useState([]);
+  const [isParseLoading, setIsParseLoading] = useState(false);
+  const [status, setStatus] = useState({});
+  const [allDownloading, setAllDownloading] = useState(false);
+  useEffect(()=>{
+    init()
+  },[])
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="App">
+      <Space className="App-topbar">
+        <span>类型</span>
+        <Popover
+          placement="bottomLeft"
+          content={
+            <div style={{ maxWidth: 520, wordBreak: "break-all" }}>
+              <p>单个视频：如 “9.94 Eho:/ 我把事情拖到最后一分钟做不是因为我懒而是那个时候我更老了 做事情也更成熟了# 叮叮当当舞 # 杰星编舞 https://v.douyin.com/2vLYnCp/ 复制此链接，打开Dou音搜索，直接观看视频！”</p>
+              <hr />
+              <p>用户所有视频：如“https://www.douyin.com/user/MS4wLjABAAAABsXrboCFzZqd2HrqUMBCUmMWRHDqjMdrW0WndNDaFAbO924AWWF7fk8YJUdZYmjk”</p>
+            </div>
+          }
+          trigger="hover"
+        >
+          <QuestionCircleOutlined />
+        </Popover>
+        <Select
+          value={parseType}
+          disabled={false}
+          onChange={(value) => setParseType(value)}
+        >
+          <Select.Option key="video">单 个 视 频</Select.Option>
+          <Select.Option key="userVideo">用户所有视频</Select.Option>
+        </Select>
+        <Input
+          placeholder={ parseType === "video" ? "请填入分享的视频链接" : "请填入用户的页面网址" }
+          disabled={false}
+          value={url}
+          onChange={({ target }) => { setUrl(target.value); }}
+        ></Input>
+        <Button
+          type="primary"
+          loading={isParseLoading}
+          onClick={async () => {
+            setIsParseLoading(true);
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+            try {
+              if (parseType === "video") {
+                const id = await invoke("get_url_id", { addr: url });
+                const info = await invoke("get_video_info_by_id", { id });
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+                setVideoInfo([info]);
+              } else {
+                const { video_count, uid } = await invoke("get_user_info_by_url", { addr: url, }) as unknown as any;
+                const info = await invoke("get_list_by_user_id", { uid, count: video_count, maxCursor: 0 }) as unknown as any;
 
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet(name)}>
-            Greet
-          </button>
+                setVideoInfo(info);
+              }
+            } catch (error: any) {
+              message.error(error);
+            }
+
+            setIsParseLoading(false);
+          }}
+        >
+          解析{parseType === "video" ? "单个视频" : "所有视频"}
+        </Button>
+        <Button
+          icon={<CloseCircleFilled />}
+          onClick={() => {
+            setUrl('')
+            setVideoInfo([])
+            // get_url_id('Hello')
+          }}
+        >
+          <b> Clear</b>
+        </Button>
+      </Space>
+
+      {videoInfo?.length > 0 ? (
+        <>
+          <div>
+            <Button
+              loading={allDownloading}
+              icon={<CloudDownloadOutlined />}
+              type="primary"
+              ghost
+              onClick={async () => {
+                const dir = await open({ directory: true });
+
+                if (!dir) {
+                  return;
+                }
+
+                setAllDownloading(true);
+
+                for (let _index = 0; _index < videoInfo.length; _index++) {
+                  const { id, title, url } = videoInfo[_index];
+                  const fileName = `${title}${Date.now()}.mp4`;
+
+                  try {
+                    setStatus((status) => ({
+                      ...status,
+                      [id]: {
+                        status: "downloading",
+                      },
+                    }));
+
+                    const filePath = await invoke("download_video", {
+                      url,
+                      writePath: dir,
+                      fileName,
+                      id: id,
+                    });
+
+                    setStatus((status) => ({
+                      ...status,
+                      [id]: {
+                        status: "done",
+                        filePath,
+                      },
+                    }));
+                  } catch (error: any) {
+                    message.error(error);
+                    setStatus({
+                      ...status,
+                      [id]: null,
+                    });
+                  }
+                }
+
+                setAllDownloading(false);
+              }}
+            >
+              全部下载
+            </Button>
+          </div>
+          <Table
+            sticky
+            rowKey="id"
+            dataSource={videoInfo}
+            columns={[
+              {
+                title: "序号",
+                dataIndex: "index",
+                key: "index",
+                ellipsis: true,
+                width: 80,
+                render: (_a, _b, index) => index + 1,
+              },
+              {
+                title: "封面",
+                dataIndex: "cover",
+                key: "cover",
+                render: (value) =>
+                  value ? (
+                    <img
+                      style={{ maxHeight: 100, maxWidth: 100 }}
+                      src={value}
+                    />
+                  ) : null,
+                width: 100,
+              },
+              {
+                title: "标题",
+                dataIndex: "title",
+                key: "title",
+                ellipsis: true,
+              },
+              {
+                title: "分辨率",
+                dataIndex: "ratio",
+                key: "ratio",
+                width: 100,
+                ellipsis: true,
+              },
+              {
+                title: "操作",
+                dataIndex: "action",
+                key: "action",
+                width: "180px",
+                render: (_, { url, title, id }) => (
+                  <div>
+                    {status[id]?.status == "done" ? (
+                      <Button
+                        icon={<EyeOutlined />}
+                        type="primary"
+                        onClick={() => {
+                          status[id].filePath &&
+                            openFile(status[id].filePath).catch(() => {});
+                        }}
+                        size="small"
+                        ghost
+                      >
+                        查看
+                      </Button>
+                    ) : (
+                      <Button
+                        icon={<DownloadOutlined />}
+                        loading={
+                          status[id]?.status == "downloading" || allDownloading
+                        }
+                        type="primary"
+                        size="small"
+                        onClick={async () => {
+                          const fileName = `${title}${Date.now()}.mp4`;
+                          const dir = await open({ directory: true });
+
+                          if (!dir) {
+                            return;
+                          }
+
+                          try {
+                            setStatus({
+                              ...status,
+                              [id]: {
+                                status: "downloading",
+                              },
+                            });
+
+                            const filePath = await invoke("download_video", {
+                              url,
+                              writePath: dir,
+                              fileName,
+                              id: id,
+                            });
+
+                            setStatus({
+                              ...status,
+                              [id]: {
+                                status: "done",
+                                filePath,
+                              },
+                            });
+                          } catch (error: any) {
+                            message.error(error);
+                            setStatus({
+                              ...status,
+                              [id]: null,
+                            });
+                          }
+                        }}
+                      >
+                        下载
+                      </Button>
+                    )}
+                    &nbsp; &nbsp;
+                    <Button
+                      icon={<PlaySquareOutlined />}
+                      onClick={() => open_url(url)}
+                      size="small"
+                    >
+                      预览
+                    </Button>
+                  </div>
+                ),
+              },
+            ]}
+            pagination={false}
+          ></Table>
+        </>
+      ) : (
+        <div className="App-logo">
+          <img src={imgLogo} />
+          <Button
+            icon={<GithubFilled />}
+            size="large"
+            onClick={() =>
+              open_url("https://github.com/ww930912/tauri_app")
+            }
+          >
+            <b> Star</b>
+          </Button>
         </div>
-      </div>
-      <p>{greetMsg}</p>
+      )}
+      <BackTop style={{ left: 50 }} />
     </div>
   );
 }
 
-export default App;
+function open_url(url) {
+  const el = document.createElement("a");
+  el.style.display = "none";
+  el.setAttribute("target", "_blank");
+  el.href = url;
+  document.body.appendChild(el);
+  el.click();
+  document.body.removeChild(el);
+}
